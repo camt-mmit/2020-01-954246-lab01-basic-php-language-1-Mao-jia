796fbb7a99391251676408ab36948fb573bbdaff
Mao-jia <68387860+Mao-jia@users.noreply.github.com>
2020-10-30 23:39:33 +0800

    Add files via upload

---
+++ practice1.php
@@ -0,0 +1,11 @@
+<?php
+/**
+     * ID:602110189
+     * Name: Mao Jia
+     * WeChat: Ga
+     */
+printf("input your price and discount%:");
+fscanf(STDIN, "%d %d", $price,$discount);
+$decrease=$price*$discount/100;
+$net=$price-$decrease;
+printf("price: %.2f, discount: %.2f, net price: %.2f",$price,$discount,$net);
